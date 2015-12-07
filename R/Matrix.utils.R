ba4655f533a16be8fe3270c28fc7aaa5444daf04
Craig Varrichio <craig_varrichio@dell.com>
2015-12-07 10:37:47 -0600

    Nearly complete 0.5 version.  Additional documentation.

--- R/Matrix.utils.R
+++ R/Matrix.utils.R
@@ -1,53 +1,80 @@
-#' Row-based functions for R objects
-#'
-#' Rowr allows the manipulation of R objects as if they were organized rows in a
-#' way that is familiar to people used to working with databases.  It allows
-#' more consistent and predictable output to common functions, and generalizes a
-#' number of utility functions to to be failsafe with any number of objects.
+#' Data.frame-like operations on sparse and dense \code{Matrix} objects,
+#' 
+#' Implements cast, aggregate, and join for \code{\link{Matrix}} and matrix-like
+#' objects.
+#' 
 #' @name Matrix.utils
 #' @docType package
+#' @import Matrix
+#' @importFrom stats aggregate
 NULL
 
-
-#' Robust alternative to \code{\link{Vectorize}} function that accepts any function with two 
-#' or more arguments.  Returns a function that will work an arbitrary number of vectors, lists or 
-#' data frames, though output may be unpredicatable in unusual applications.  The 
-#' results are also intended to be more intuitive than Vectorize.
+#' Casts or pivots a long \code{data frame} into a wide sparse matrix.
 #' 
-#' @param fun a two or more argument function
-#' @param type like \code{MARGIN} in \code{\link{apply}}, except that \code{c(1,2)} is
-#'   represented as a \code{3} instead.  By default, will \code{Reduce} single dimensional
-#'   data handle everything else row-wise.
+#' Similar in function to \code{\link[reshape2]{dcast}}, but produces a sparse 
+#' \code{\link{Matrix}} as an output. Sparse matrices are beneficial for this 
+#' application because such outputs are often very wide and sparse. Conceptually
+#' similar to a \code{pivot} operation.
+#' 
+#' Casting formulas are slightly different than those in \code{dcast} and follow
+#' the conventions of \code{\link{model.matrix}}. See \code{\link{formula}} for 
+#' details.  Briefly, the left hand side of the \code{~} will be used as the 
+#' grouping criteria.  This can either be a single variable, or a group of 
+#' variables linked using \code{:}.  The right hand side specifies what the 
+#' columns will be. Unlike \code{dcast}, using the \code{+} operator will append
+#' the values for each variable as additional columns.  This is useful for 
+#' things such as one-hot encoding.  Using \code{:} will combine the columns as 
+#' interactions.
+#' 
+#' @param data a data frame
+#' @param formula casting \code{\link[stats]{formula}}, see details for
+#'   specifics.
+#' @param fun.aggregate name of aggregation function.  Defaults to 'sum'
+#' @param value.var name of column that stores values to be aggregated
+#' @seealso \code{\link[reshape]{cast}}
+#' @seealso \code{\link[reshape2]{dcast}}
+#' @seealso \code{\link[data.table]{dcast.data.table}}
 #' @export
 #' @examples
-#' ## Not run:
-#' orders<-data.frame(orderNum=as.factor(sample(1e6, 1e7, TRUE)),
-#'    sku=as.factor(sample(1e3, 1e7, TRUE)),
-#'    customer=as.factor(sample(1e4,1e7,TRUE)),
-#'    state = sample(letters, 1e7, TRUE), amount=runif(1e7))
-#' # For simple aggregations resulting in small tables, dcast.data.table (and even reshape2) will be faster
-#' system.time(a<-dcast.data.table(as.data.table(orders),sku~state,sum,value.var = 'amount')) # .5 seconds
-#' system.time(b<-reshape2::dcast(orders,sku~state,sum,value.var = 'amount')) # 2.61 seconds
-#' system.time(c<-dcast.Matrix(orders,sku~state,value.var = 'amount')) # 28 seconds
-#' # However, this situation changes as the result set becomes larger
-#' system.time(a<-dcast.data.table(as.data.table(orders),customer~sku,sum,value.var = 'amount')) #4 .4 seconds
-#' system.time(b<-reshape2::dcast(orders,customer~sku,sum,value.var = 'amount')) # 34.7 seconds
-#' system.time(c<-dcast.Matrix(orders,customer~sku,value.var = 'amount')) # 27 seconds
-#' # More complicated:
-#' system.time(a<-dcast.data.table(as.data.table(orders),customer~sku+state,sum,value.var = 'amount')) # 18.1 seconds, object size = 2084 Mb
-#' system.time(b<-reshape2::dcast(orders,customer~sku+state,sum,value.var = 'amount')) # Does not return
-#' system.time(c<-dcast.Matrix(orders,customer~sku:state,value.var = 'amount')) # 30.69 seconds, object size = 115.4 Mb
 #' 
-#' system.time(a<-dcast.data.table(as.data.table(orders),orderNum~sku,sum,value.var = 'amount')) # Does not return
-#' system.time(c<-dcast.Matrix(orders,orderNum~sku,value.var = 'amount')) # 36.33 seconds, object size = 175Mb
+#' \dontrun{
+#' orders<-data.frame(orderNum=as.factor(sample(1e6, 1e7, TRUE)), 
+#'    sku=as.factor(cast.Matrsample(1e3, 1e7, TRUE)), 
+#'    customer=as.factor(sample(1e4,1e7,TRUE)), 
+#'    state = sample(letters, 1e7, TRUE),
+#'    amount=runif(1e7)) 
+#' # For simple aggregations resulting in small tables, dcast.data.table (and
+#' # even reshape2) will be faster
+#' system.time(a<-dcast.data.table(as.data.table(orders),sku~state,sum,
+#'    value.var = 'amount')) # .5 seconds 
+#' system.time(b<-reshape2::dcast(orders,sku~state,sum,
+#'    value.var = 'amount')) # 2.61 seconds 
+#' system.time(c<-cast.Matrix(orders,sku~state,
+#'    value.var = 'amount')) # 28 seconds 
+#'    
+#' # However, this situation changes as the result set becomes larger 
+#' system.time(a<-dcast.data.table(as.data.table(orders),customer~sku,sum,
+#'    value.var = 'amount')) # 4.4 seconds 
+#' system.time(b<-reshape2::dcast(orders,customer~sku,sum,
+#'    value.var = 'amount')) # 34.7 seconds 
+#'  system.time(c<-cast.Matrix(orders,customer~sku,
+#'    value.var = 'amount')) # 27 seconds 
+#'    
+#' # More complicated: 
+#' system.time(a<-dcast.data.table(as.data.table(orders),customer~sku+state,sum,
+#'    value.var = 'amount')) # 18.1 seconds, object size = 2084 Mb 
+#' system.time(b<-reshape2::dcast(orders,customer~sku+state,sum,
+#'    value.var = 'amount')) # Does not return 
+#' system.time(c<-cast.Matrix(orders,customer~sku:state,
+#'    value.var = 'amount')) # 30.69 seconds, object size = 115.4 Mb
 #' 
-#' system.time(c<-dcast.Matrix(orders,orderNum~customer,value.var = 'amount')) # 31.28 seconds, object size = 176Mb
+#' system.time(a<-dcast.data.table(as.data.table(orders),orderNum~sku,sum,
+#'    value.var = 'amount')) # Does not return 
+#' system.time(c<-cast.Matrix(orders,orderNum~sku,
+#'    value.var = 'amount')) # 36.33 seconds, object size = 175Mb
 #' 
-#' 
-dcast.Matrix<-function(data,formula,value.var=NULL,fun='sum')
+cast.Matrix<-function(data,formula,fun.aggregate='sum',value.var=NULL)
 {
-  require(dplyr)
-  #browser()
   alltms<-terms(formula,data=data)
   response<-rownames(attr(alltms,'factors'))[attr(alltms,'response')]
   tm<-attr(alltms,"term.labels")
@@ -59,7 +86,13 @@ dcast.Matrix<-function(data,formula,value.var=NULL,fun='sum')
   newterms<-c(simple,newterms)
   newformula<-as.formula(paste(response,'~0+',paste(newterms,collapse='+')))
   result<-sparse.model.matrix(newformula,data,drop.unused.levels = TRUE)
-  colnames(result)<-lapply(colnames(result),function (x) gsub('paste(',replacement='',x=x,fixed = TRUE) %>% gsub(pattern=', ',replacement='_',x=.,fixed=TRUE) %>% gsub(pattern='_sep = \"_\")',replacement='',x=.,fixed=TRUE))
+  brokenNames<-grep('paste(',colnames(result),fixed = TRUE)
+  colnames(result)[brokenNames]<-lapply(colnames(result)[brokenNames],function (x) {
+    x<-gsub('paste(',replacement='',x=x,fixed = TRUE) 
+    x<-gsub(pattern=', ',replacement='_',x=.,fixed=TRUE) 
+    x<-gsub(pattern='_sep = \"_\")',replacement='',x=.,fixed=TRUE)
+    return(x)
+  })
   values<-1
   if(!is.null(value.var))
     values<-data[,value.var]
@@ -72,48 +105,119 @@ dcast.Matrix<-function(data,formula,value.var=NULL,fun='sum')
   return(result)
 }
 
+#' Compute summary statistics of a Matrix.
+#' 
+#' Similar to \code{\link[stats]{aggregate}}.  Splits the matrix into groups as
+#' specified by groupings, which can be one or more variables. Aggregation
+#' function will be applied to all columns in data, or as specified in formula. 
+#' Warning: groupings will be made dense if it is sparse, though data will not.
+#' 
+#' @param x a \code{\link{Matrix}} or matrix-like object
+#' @param groupings an object coercible to a group of factors defining the groups
+#' @param form \code{\link[stats]{formula}}
+#' @param fun name of aggregation function to be applied to all columns in data
+#' @seealso \code{\link[dplyr]{summarise}}
+#' @seealso \code{\link[plyr]{summarise}}
+#' @seealso \code{\link[stats]{aggregate}}
+#' @export
+#' @export aggregate.Matrix
 #' @examples
-#' ## Not run:
+#' skus<-Matrix(as.matrix(data.frame(
+#'    orderNum=sample(1000,10000,TRUE),
+#'    sku=sample(1000,10000,TRUE),
+#'    amount=runif(10000))),sparse=TRUE)
+#' a<-aggregate.Matrix(skus[,'amount'],skus[,'sku',drop=FALSE])
+#' 
+#' m<-rsparsematrix(1000000,100,.001)
+#' labels<-as.factor(sample(1e4,1e6,TRUE))
+#' b<-aggregate.Matrix(m,labels)
+#' 
+#' \dontrun{
+#' orders<-data.frame(orderNum=as.factor(sample(1e6, 1e7, TRUE)),
+#'    sku=as.factor(sample(1e3, 1e7, TRUE)),
+#'    customer=as.factor(sample(1e4,1e7,TRUE)),
+#'    state = sample(letters, 1e7, TRUE), amount=runif(1e7))
 #' system.time(d<-aggregate.Matrix(orders[,'amount',drop=FALSE],orders$orderNum))
-aggregate.Matrix<-function(data,groupings=NULL,form=NULL,fun='sum')
+#' }
+aggregate.Matrix<-function(x,groupings=NULL,form=NULL,fun='sum')
 {
-  browser()
-  if(!is(data,'Matrix'))
-    data<-Matrix(as.matrix(data),sparse=TRUE)
+#   if(!is(x,'Matrix'))
+#     x<-Matrix(as.matrix(x),sparse=TRUE)
   if(fun=='count')
-    data<-data!=0
+    x<-x!=0
+  if(is(groupings,'Matrix'))
+    groupings<-as.matrix(groupings)
   groupings<-data.frame(groupings)
   groupings<-data.frame(lapply(groupings,as.factor))
   if(is.null(form))
     form<-as.formula('~0+.')
   form<-as.formula(form)
-  mapping<-dcast.Matrix(groupings,form)
-  result<-t(mapping) %*% data
+  mapping<-cast.Matrix(groupings,form)
+  result<-t(mapping) %*% x
   if(fun=='mean')
-    result@x<-result@x/(aggregate.Matrix(data,groupings,fun='count'))@x
+    result@x<-result@x/(aggregate.Matrix(x,groupings,fun='count'))@x
   return(result)
 }
 
-#' Joins two matrices
+#'Joins two matrices.
+#'
+#'Similar to \code{\link{merge}} or Rudimentary at this point; can do left and 
+#'inner joins that only find the first matching row; equivalent to 
+#'plyr::join(match='first').
+#'
+#'@param x \code{\link{Matrix}} or matrix-like object
+#'@param y \code{Matrix} or matrix-like object
+#'@param by.x vector indicating the names to match from \code{Matrix} x
+#'@param by.y vector indicating the names to match from \code{Matrix} y
+#'@param type type of join: currently on left and inner are supported
+#'@export
 #' @examples
-#' orders<-Matrix(as.matrix(data.frame(orderNum=1:1000,customer=sample(100,1000,TRUE))),sparse=TRUE)
-#' cancelledOrders<-Matrix(as.matrix(data.frame(orderNum=sample(1000,100),cancelled='Y)),sparse=TRUE)
-#' skus<-Matrix(as.matrix(data.frame(orderNum=sample(1000,10000,TRUE),sku=sample(1000,10000,TRUE),amount=runif(10000))),sparse=TRUE)
-join.Matrix<-function(x,y,rownames.x=rownames(x),rownames.y=rownames(y))
+#' orders<-Matrix(as.matrix(data.frame(orderNum=1:1000,
+#'    customer=sample(100,1000,TRUE))),sparse=TRUE)
+#' cancelledOrders<-Matrix(as.matrix(data.frame(orderNum=sample(1000,100),
+#'    cancelled=1)),sparse=TRUE)
+#' skus<-Matrix(as.matrix(data.frame(orderNum=sample(1000,10000,TRUE),
+#'    sku=sample(1000,10000,TRUE),
+#'    amount=runif(10000))),sparse=TRUE)
+#' a<-join.Matrix(orders,cancelledOrders,orders[,'orderNum'],cancelledOrders[,'orderNum'])
+#' b<-join.Matrix(orders,cancelledOrders,orders[,'orderNum'],cancelledOrders[,'orderNum'],type='inner')
+#' c<-join.Matrix(orders,skus,orders[,'orderNum'],skus[,'orderNum'])
+#' 
+#' \dontrun{
+#' ## Not run:
+#' orders<-data.frame(orderNum=sample(1e6, 1e7, TRUE),
+#'    sku=sample(1e3, 1e7, TRUE),
+#'    customer=sample(1e4,1e7,TRUE))
+#' cancelledOrders<-data.frame(data.frame(orderNum=sample(1e6,1e5),cancelled=1))
+#' system.time(b<-join.Matrix(orders,cancelledOrders,orders[,'orderNum'],cancelledOrders[,'orderNum'],type='inner'))
+#' #The following is the equivalent call in plyr, but returns an error due to a bug in plyr
+#' system.time(c<-plyr::join(orders,cancelledOrders,type='inner',match='first')) 
+#'}
+join.Matrix<-function(x,y,by.x=rownames(x),by.y=rownames(y),type='left')
 {
-  browser()
-#   rownames.x<-as.character(rownames.x)
-#   rownames.y<-as.character(rownames.y)
-#   rownames(x)<-rownames.x
-#   rownames(y)<-rownames.y
-  #inter<-intersect(rownames.x,rownames.y)
-  #y<-y[inter,,drop=FALSE]
-  indices1<-match(rownames.x,rownames.y)
+  indices1<-match(by.x,by.y)
   indices1[is.na(indices1)]<-nrow(y)+1
   y2<-rbind2(y,NA)[indices1,]
   colnames(y2)[colnames(y2) %in% colnames(x)]<-paste('y',colnames(y2)[colnames(y2) %in% colnames(x)])
   result<-cbind2(x,y2)
-  #indices2<-match(rownames.y,rownames.x)
-  #result<-cbind2(x,y[rownames.x,])
+  if(type=='inner')
+    result<-result[!(indices1==(nrow(y)+1)),]
   return(result)
 }
+
+melt<-function(data,idColumns)
+{
+  cols<-setdiff(colnames(data),idColumns)
+  results<-lapply(cols,function (x) cbind(data[,idColumns],variable=x,value=data[,x]))
+  results<-Reduce(rbind,results)
+}
+
+
+#' Synonym for cast.Matrix.
+#' @rdname cast.Matrix
+#' @export
+acast.Matrix<-cast.Matrix
+#' Synonym for cast.Matrix.  
+#' @rdname cast.Matrix
+#' @export
+dcast.Matrix<-cast.Matrix
